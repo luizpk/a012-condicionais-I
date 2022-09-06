30370f9bca04d5c2f6b78c1b18ad7cda87788457
LuizPK <luizpkpinheiro@gmail.com>
2022-09-06 14:56:41 -0300

    Resolvendo exercícios aula 12 Condicionais-I

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,6 @@
-const num = //seu numero aqui
+//Crie um código que receba uma const numérica qualquer. Crie um `if` para verificar se o número guardado na const é **par**. Caso seja, imprima no console a mensagem: "o número é par".
 
-if(){
-    console.log("sua mensagem aqui");
+const num = 64
+if(num%2==0){
+    console.log(`O número ${num} é um número par.`);
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,27 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
 
-if(){
 
+let nome = prompt("Digite seu nome")
+let idade = Number(prompt("Digite sua idade."));
+if(idade>=18){
+    alert(`${nome}, vocé é maior de idade!`)
 }else{
-	
+	alert(`${nome}, você ainda não atingiu a maioridade.`)
 }
+
+
+let terminouEM = prompt(`${nome}, você terminou o Ensino Médio?
+Digite Sim ou Não`)
+if(terminouEM===("Sim"&&"sim")){
+    alert(`Parabêns ${nome}! 
+           Você tem o Ensino Médio concluído!`)
+    let isCursandoOutraFaculdade = ( prompt(`${nome}, Você está cursando Ensino Superior?
+    Digite Sim ou Não`))
+     if(isCursandoOutraFaculdade===("Sim"&&"sim")){
+        alert("Isso mesmo, você está no caminho certo!")
+     } else{
+        alert(`${nome}, você precisa completar seus estudos.`)
+     }      
+}else{
+	alert(`${nome}, você precisa completar seus estudos.`)
+}
+
